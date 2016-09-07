6f211ba84eb042b4f1babb6609eaad1ac4a5968b
LYAbdala <abdalaleila@gmail.com>
2016-09-07 14:41:06 -0300

    lalala

--- src/spam_filter.py
+++ src/spam_filter.py
@@ -12,8 +12,8 @@ import sys
 ###Consejo: correr esto en una máquina con >4GB de memoria
 
 # Leo los mails (poner los paths correctos).
-ham_txt= json.load(open('../dataset_json/ham_dev.json'))
-spam_txt= json.load(open('../dataset_json/spam_dev.json'))
+ham_txt= json.load(open('../dataset_dev/ham_dev.json'))
+spam_txt= json.load(open('../dataset_dev/spam_dev.json'))
 
 df = pd.DataFrame(ham_txt+spam_txt, columns=['text'])
 df['class'] = ['ham' for _ in range(len(ham_txt))]+['spam' for _ in range(len(spam_txt))]
@@ -26,11 +26,17 @@ print "Lei json"
 
 def ranking(mails, k):
   #Limpio etiquetas html
-  mails = [re.sub('<[^<]+?>', '', mail) for mail in mails]
+  # l = [re.sub('<[^<]+?>', '', mail) for mail in mails]
+
+  l = []
+  for mail in mails:
+    mail = " ".join(mail)
+    l.append(re.sub('<.*>', '', mail))
+
   #Hago una lista con todas las palabras que aparecen (con repeticiones)
   #sin signos de puntuacion ni chirimbolos raros 
   mailWords = []
-  for mail in mails:
+  for mail in l:
     for w in re.findall(r'[a-z]+', mail):
       mailWords.append(w)
 
@@ -54,12 +60,49 @@ def setDifference(spam, ham):
 def countGreaterThan(text, word, threshold):
   return text.count(word) >=  threshold
 
-spam_words = ranking(spam_txt, 400)
-ham_words = ranking(ham_txt, 400)
+def filtrarMugresGenerico(mailList, bodys, subjects, froms, tos):
+  for mail in mailList:
+    mail_lines = mail.split('\n')
+    for line_index in range(len(mail_lines)):
+      line = mail_lines[line_index]
+      if line[:5] == "from:":
+        froms.append(line[6:])
+      elif line[:8] == "subject:":
+        subjects.append(line[9:])
+      elif line[:3] == "to:":
+        tos.append(line[4:])
+      elif line == "\r":
+        bodys.append(mail_lines[line_index:])
+        break
+
+# def frecuenciasPromedio(spamList, words):
+
+body_spam = []
+subjetc_spam = []
+from_spam = []
+to_spam = []
+filtrarMugresGenerico(spam_txt, body_spam, subjetc_spam, from_spam, to_spam)
+
+body_ham = []
+subjetc_ham = []
+from_ham = []
+to_ham = []
+filtrarMugresGenerico(ham_txt, body_ham, subjetc_ham, from_ham, to_ham)
+
+body_spam_words = ranking(body_spam, 400)
+
+body_ham_words = ranking(body_ham, 400)
+
 words = []
-for word, occur in setDifference(spam_words, ham_words):
+for word, occur in setDifference(body_spam_words, body_ham_words):
+  print word, occur
   if word == 'html': continue
-  df[word] = map(lambda x: countGreaterThan(x, word, 1), df.text)
+  df[word] = []
+  for mail in df.text:
+    if word == 'base':
+      print mail
+    df[word].append(countGreaterThan(mail, word, 1)) 
+  # df[word] = map(lambda x: countGreaterThan(x, word, 1), df.text)
   words.append(word)
 
 
@@ -67,6 +110,9 @@ words.append('html')
 # Preparo data para clasificar
 X = df[words].values
 y = df['class']
+
+
+sys.exit()
 
 # Elijo mi clasificador.
 clf = DecisionTreeClassifier(criterion='entropy')
